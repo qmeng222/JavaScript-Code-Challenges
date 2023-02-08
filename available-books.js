d4501fd27fd7efce7ea599dd0260fc0b87b16cc1
Qingying Meng <qmeng222@gmail.com>
2023-02-08 13:27:11 -0800

    updated available books with class syntax

--- available-books.js
+++ available-books.js
@@ -1,29 +1,63 @@
-function Book(title, author, ISBN, numCopies) {
-  this.title = title;
-  this.author = author;
-  this.ISBN = ISBN;
-  this.numCopies = numCopies;
+// // 1. use function syntax to create prototype:
+// function Book(title, author, ISBN, numCopies) {
+//   this.title = title;
+//   this.author = author;
+//   this.ISBN = ISBN;
+//   this.numCopies = numCopies;
+// }
 
-  // NOTE: each instance can use the prototype's function without creating a new function✊
-  // func 1:
-  Book.prototype.getAvailability = function () {
+// // NOTE: each instance can use the prototype's function without creating a new function✊
+// // func 1:
+// Book.prototype.getAvailability = function () {
+//   if (this.numCopies === 0) {
+//     return "💥 Out of stock";
+//   } else if (this.numCopies < 10) {
+//     return "❓Low stock";
+//   }
+//   return "✅ In stock";
+// };
+
+// // func 2: subtract sold from the total number of copies, if no argument is passed, default the number of copies to sell to 1
+// Book.prototype.sell = function (numCopiesSold = 1) {
+//   this.numCopies -= numCopiesSold;
+// };
+
+// // func 3: takes in a number of copies to restock and add it it to the total number of copies; if no argument is passed, default the restock number to 5
+// Book.prototype.restock = function (numCopiesStocked = 5) {
+//   this.numCopies += numCopiesStocked;
+// };
+
+// 2. use class syntax to create prototype:
+class Book {
+  // NOTE: all book-related bode lives inside the book class (encapsulation)
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  // use a getter function to retrieve a book's availability instead of calling book.getAvailability():
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
     if (this.numCopies === 0) {
       return "💥 Out of stock";
     } else if (this.numCopies < 10) {
       return "❓Low stock";
     }
     return "✅ In stock";
-  };
+  }
 
-  // func 2: subtract sold from the total number of copies, if no argument is passed, default the number of copies to sell to 1
-  Book.prototype.sell = function (numCopiesSold = 1) {
+  sell(numCopiesSold = 1) {
     this.numCopies -= numCopiesSold;
-  };
+  }
 
-  // func 3: takes in a number of copies to restock and add it it to the total number of copies; if no argument is passed, default the restock number to 5
-  Book.prototype.restock = function (numCopiesStocked = 5) {
+  restock(numCopiesStocked = 5) {
     this.numCopies += numCopiesStocked;
-  };
+  }
 }
 
 // test:
